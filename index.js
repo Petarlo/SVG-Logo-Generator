b24a2b5b7e207a4be13cf55583021dadc9360845
peta swan <petaswan986@gmail.com>
2023-10-30 15:56:26 +1100

    code push to lib file - questions, shapes, shapes.test - and index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./lib/questions.js');
 const fileName = "./examples/logo.svg";
-const setShape = require('./lib/setShape.js')
+const { Triangle, Square, Circle } = require("./lib/shapes.js");
 
 // function to create new svg file using inquirer response and file system
 function createLogo(response) {
